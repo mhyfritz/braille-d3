7679e182c57fcedab12ebac7d8970d583d51b9ae
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2018-02-09 21:46:08 +0100

    split codes into categories `letter` and `number`

--- script.js
+++ script.js
@@ -4,27 +4,43 @@ d3.json('data.json', data => {
 
 function vis(codes) {
   const chart = d3.select('#vis')
-
-  for (const code of codes) {
-    const container = chart.append('div')
-      .attr('class', 'container')
-
-    container.append('h1')
-      .text(code.character)
-
-    container.append('svg')
-      .attr('width', 80)
-      .attr('height', 100)
-      .selectAll('circle')
-      .data(code.dots)
-      .enter()
-      .append('circle')
-      .attr('cx', (d, i) => (i % 2 + 1) * 20)
-      .attr('cy', (d, i) => `${(Math.floor(i / 2) + 1) * 20}px`)
-      .attr('r', '3px')
-      .transition()
-      .duration(400)
-      .delay(400)
-      .attr('r', d => `${(d * 3) + 3}px`)
+  for (const { key, group } of groupby(codes, code => code.category)) {
+    chart.append('h2').text((key + 's').toUpperCase())
+    for (const code of group) {
+      const container = chart.append('div')
+        .attr('class', 'container')
+      container.append('h3')
+        .text(code.symbol)
+      container.append('svg')
+        .attr('width', 80)
+        .attr('height', 100)
+        .selectAll('circle')
+        .data(code.dots)
+        .enter()
+        .append('circle')
+        .attr('cx', (d, i) => (i % 2 + 1) * 20)
+        .attr('cy', (d, i) => `${(Math.floor(i / 2) + 1) * 20}px`)
+        .attr('r', '3px')
+        .transition()
+        .duration(400)
+        .delay(400)
+        .attr('r', d => `${(d * 3) + 3}px`)
+    }
   }
+}
+
+function groupby (iterable, keyfunc) {
+  const ret = []
+  for (const elem of iterable) {
+    const key = keyfunc(elem)
+    if (ret.length === 0 || ret[ret.length - 1].key !== key) {
+      ret.push({
+        key,
+        group: [elem]
+      })
+    } else {
+      ret[ret.length - 1].group.push(elem)
+    }
+  }
+  return ret
 }
