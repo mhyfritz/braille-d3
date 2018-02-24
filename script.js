98632cad0aa18217ff458ce61279d90928b75713
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2018-02-24 16:11:12 +0100

    use `easeBackOut` easing function and add header

--- script.js
+++ script.js
@@ -22,8 +22,9 @@ function vis(codes) {
         .attr('cy', (d, i) => `${(Math.floor(i / 2) + 1) * 20}px`)
         .attr('r', '3px')
         .transition()
+        .ease(d3.easeBackOut)
         .delay(1000)
-        .duration(400)
+        .duration(1000)
         .attr('r', d => `${(d * 3) + 3}px`)
     }
   }
